cc07cf60f67696de5e8642a33ff462c6cfb7f493
Gordon Ram <108928303+GordonRam1@users.noreply.github.com>
2025-10-07 22:33:24 +0530

    Update EvenOddXor.c

--- EvenOddXor.c
+++ EvenOddXor.c
@@ -4,7 +4,9 @@ int main()
     int n;
     printf("Enter number to check if even or odd : ");
     scanf("%d",&n);
+    // Here i am using the and operator to check if the number is even or odd.
     if((n^1)==n+1)
+
     {
         printf("Even");
     }
@@ -12,5 +14,6 @@ int main()
     {
         printf("Odd");
     }
+
 
 }
